d0c159578a74c9282757dcc8cd630da3ba0840fe
Vitali Tereshchenkov <vitalikgaga@gmail.com>
2018-01-14 20:10:59 +0300

    toObject method to hide __v

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -8,6 +8,17 @@ const beautifyUnique = require('mongoose-beautiful-unique-validation');
 mongoose.plugin(beautifyUnique);
 mongoose.set('debug', true);
 
+mongoose.plugin(schema => {
+    if (!schema.options.toObject) {
+      schema.options.toObject = {};
+    }
+  
+    if (schema.options.toObject.transform == undefined) {
+      schema.options.toObject.transform = (doc, ret) => { delete ret.__v; return ret; };
+    }
+  
+  });
+
 mongoose.connect('mongodb://localhost/test', {
     server: {
         socketOptions: {
